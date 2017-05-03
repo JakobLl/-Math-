bf224b460be3c0262020129f564a9ce08b8be6b5
JakobLl <jakob_boii@outlook.com>
2017-05-03 14:09:29 -0500

    Simple square root calculator

---
+++ SquareRoot.cpp
@@ -0,0 +1,19 @@
+```
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+
+using namespace std;
+int main (){
+     double param, result,  enter;
+    cout << " Enter Value: ";
+    cin >> enter;
+    param = enter; 
+    result = sqrt (param); 
+    printf (" The square root of %f is: \n %f\n",param,  round(result ));
+   return 0; 
+}
+
+```
